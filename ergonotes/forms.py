03a2d9b07e24d5c12a6cbe7b947314cd37e9fb2f
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-03-27 15:07:53 -0300

    Update for foundation forms

--- ergonotes/forms.py
+++ ergonotes/forms.py
@@ -3,11 +3,20 @@
 from __future__ import unicode_literals
 
 from django import forms
+from ergo.foundation.forms import FoundationForm
 
 from . import models
 
 
-class NoteForm(forms.ModelForm):
+class NoteForm(forms.ModelForm, FoundationForm):
     class Meta:
         fields = ('title', 'priority', 'show_on_home', 'format_type', 'text')
         model = models.Note
+
+        foundation_column_class = {
+            'title': 'small-12',
+            'priority': 'small-6 medium-4',
+            'show_on_home': 'small-6 medium-4',
+            'format_type': 'small-12 medium-4',
+            'text': 'small-12',
+        }
